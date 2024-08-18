e2437001f38389abec31bebade1258df6ca56e6a
Ruzgarkartali <89749347+Ruzgarkartali@users.noreply.github.com>
2024-08-18 14:48:51 +0200

    Init

---
+++ src/main/java/civcraft/org/example/citizens/Citizens.java
@@ -0,0 +1,17 @@
+package civcraft.org.example.citizens;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Citizens extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
